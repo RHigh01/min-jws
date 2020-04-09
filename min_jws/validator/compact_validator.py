61aebda5c736132020238dd8ffc129f34d0db72c
RHigh01 <52425062+RHigh01@users.noreply.github.com>
2020-04-08 18:05:43 -0700

    fixed whitespace

--- min_jws/validator/compact_validator.py
+++ min_jws/validator/compact_validator.py
@@ -12,7 +12,7 @@ def validate_compact(jws: bytes, validate_jose_header: JOSEValidatorFn) -> JWSPa
     try:
         # todo: line breaks?
         # 5.2.2, 5.2.6, 5.2.7
-        protected_header_bytes = custom_urlsafe_b64decode(parts[0])        
+        protected_header_bytes = custom_urlsafe_b64decode(parts[0])
     except Exception:
         raise ValueError("cannot b64decode")
 
@@ -28,10 +28,10 @@ def validate_compact(jws: bytes, validate_jose_header: JOSEValidatorFn) -> JWSPa
     # 5.2.5
     alg_fn = validate_jose_header(jose_header)
     signing_input = gen_signing_input_bytes(header=b64_utf8(jose_header), payload=encoded_payload_bytes)
-    
+
     # 5.2.8
     signature = custom_urlsafe_b64encode(alg_fn(signing_input))
     if parts[2] != signature:
         raise ValueError("signatures dont match")
-        
+
     return JWSPayloadBytes(custom_urlsafe_b64decode(encoded_payload_bytes))
